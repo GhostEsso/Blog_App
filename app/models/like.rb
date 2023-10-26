2f76496bec0b11ccfd4ea9e1dd207be3ecf2cb77
GhostEsso <lilipitaham@gmail.com>
2023-10-26 08:41:01 +0000

    linters error fixed

--- app/models/like.rb
+++ app/models/like.rb
@@ -6,7 +6,7 @@ class Like < ApplicationRecord
   validates :post_id, presence: true
 
   after_save :update_likes
-  
+
   def update_likes
     post = self.post
     post.update(likes_counter: post.likes.count)
