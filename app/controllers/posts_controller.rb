7d06680675c26bc94308b7513b322e5b6fe08bf2
GhostEsso <lilipitaham@gmail.com>
2023-10-31 15:11:31 +0000

    Make Posts and UsersControllers use standard.html.erb and declare necessary variables in Posts and Users Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  layout 'standard'
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
+    @posts = @posts.paginate(page: params[:page], per_page: 2)
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
+  layout 'standard'
+  def index
+    @users = User.order(id: :asc)
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 end
