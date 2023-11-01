e290a23b814006a13a401f2de227156226920ee5
GhostEsso <lilipitaham@gmail.com>
2023-11-01 10:27:38 +0000

    linters errors fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
-    layout 'standard'
-    def new
-      @comment = Comment.new
-    end
-  
-    def create
-      @comment = Comment.new(params.require(:comment).permit(:text, :user_id, :post_id))
-      if @comment.save
-        flash[:success] = 'Comment created!'
-        redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
-      else
-        flash.now[:error] = 'Error: Comment can not be created!'
-        render :new, locals: { comment: @comment }
-      end
+  layout 'standard'
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(params.require(:comment).permit(:text, :user_id, :post_id))
+    if @comment.save
+      flash[:success] = 'Comment created!'
+      redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
+    else
+      flash.now[:error] = 'Error: Comment can not be created!'
+      render :new, locals: { comment: @comment }
     end
   end
+end
