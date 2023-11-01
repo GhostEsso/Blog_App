8a5c74cf6949976a3e917453ff1d6f0fe750db4f
GhostEsso <lilipitaham@gmail.com>
2023-11-01 09:33:07 +0000

    current_user method created and #new #create methods created in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,28 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
-    @posts = @posts.paginate(page: params[:page], per_page: 2)
+    @posts = @posts.paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  # create new and create methods
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'Post created'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error: Post can not be created!'
+      render :new, locals: { post: @post }
+    end
   end
 end
