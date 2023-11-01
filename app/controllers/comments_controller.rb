4009af55f2547b8a7a7c9aa59fed15ebf250e902
GhostEsso <lilipitaham@gmail.com>
2023-11-01 10:12:23 +0000

    comments controller updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
       @comment = Comment.new(params.require(:comment).permit(:text, :user_id, :post_id))
       if @comment.save
         flash[:success] = 'Comment created!'
-        redirect_to "/users/#{@comment.user_id}/posts/#{@comment.post_id}"
+        redirect_to user_post_path(id: @comment.post_id, user_id: @comment.user_id)
       else
         flash.now[:error] = 'Error: Comment can not be created!'
         render :new, locals: { comment: @comment }
