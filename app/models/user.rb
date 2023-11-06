7381fc3787e8663d0fc1c7fd340d3696dad2b5ce
GhostEsso <lilipitaham@gmail.com>
2023-11-06 17:39:26 +0000

    queries on models more concise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :post, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def three_recent_posts
-    Post.where(author: self).order(created_at: :desc).first(3)
+    posts.order(created_at: :desc).first(3)
   end
 end
