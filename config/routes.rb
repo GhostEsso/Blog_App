09a7e800c1bd969174f817a0f6e0d22e1672e3d5
GhostEsso <lilipitaham@gmail.com>
2023-11-07 14:14:34 +0000

    new migration created, update User model and routes.rb by running 'rails generate devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  root "users#index"
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'users#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => 'login#index', as: :unauthenticated_root
+    end
+  end
+
+  get '/login', to: 'login#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
       resources :comments, only: [:new, :create]
