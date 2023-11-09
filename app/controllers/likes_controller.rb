05d34ae21a7a69ca0181f6a5fd1bf5edd20165a9
GhostEsso <lilipitaham@gmail.com>
2023-11-09 15:05:58 +0000

    destroy action added to LikesController to implement 'unliking' functionality, enforce strong params by defining like_params method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,32 @@
 class LikesController < ApplicationController
   layout 'standard'
   def create
-    @like = Like.new(post_id: params[:post_id], user_id: params[:user_id])
+    @like = Like.new(like_params)
     @like.user = current_user
+    @post = @like.post
     if @like.save
       flash[:success] = 'Liked!'
-      redirect_to user_post_path(id: @like.post_id, user_id: @like.user_id)
+      redirect_to user_post_path(id: params[:post_id], user_id: params[:user_id])
     else
-      flash.now[:error] = 'It seems like something went wrong!'
+      flash.now[:error] = 'You can\'t like twice!!!'
     end
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.post.decrement!(:likes_counter)
+    @post = @like.post
+    if @like.destroy!
+      flash[:success] = 'Unliked!'
+      redirect_to user_post_path(id: params[:post_id], user_id: params[:user_id])
+    else
+      flash.now[:error] = 'Ooops, something went wrong'
+    end
+  end
+
+  private
+
+  def like_params
+    params.permit(:post_id)
   end
 end
